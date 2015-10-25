3a80eccd440d4047b16ae4034766cc028e34d817
Rebecca Lieberman <rebeccalieberman@gmail.com>
2015-10-25 14:30:38 -0400

    updates to check if the images are loaded

--- imageprocess2.js
+++ imageprocess2.js
@@ -4,20 +4,20 @@ var canvas;
 var w; //set the width and height of the canvas in the browser and offscreen canvas for accessing pixels
 var h;
 
-var tileSize = 50; //size of our tiles, eventually will be dynamically controlled by a slider or form
+var tileSize = 5; //size of our tiles, eventually will be dynamically controlled by a slider or form
 var colNum; //variables to hold column and row numbers
 var rowNum;
 
 var imgURLS = []; //array to hold the URLS
 var imgElements = []; //array to hold all the images as DOM elements
 
+var imgsLoaded = false; //boolean to mark if the images are loaded as DOM elements
+
 
 function preload(){
-  imgURLS = loadStrings('guys.txt'); //for now, load image URLS from a text file so we don't overload the api
+  imgURLS = loadStrings('urls2.txt'); //for now, load image URLS from a text file so we don't overload the api
     
 }
-
-var canvas;
 
 function setup() {
 
@@ -43,18 +43,25 @@ function setup() {
   }
 
 
-//STEP 1: make DOM elements for all of the images in the array of URLS
-  for (var i=0; i<imgURLS.length-1; i++) { 
-    var imgElt = createImg("images/" + imgURLS[i]);
+//STEP 1A: make DOM elements for all of the images in the array of URLS
+  for (var i=0; i<imgURLS.length; i++) { 
+    var imgElt = createImg(imgURLS[i]);
     imgElements.push(imgElt); //
-    // imgElt.hide();
+    imgElt.hide();
     // console.log(imgElt.elt.currentSrc, i);
-
   }
-  // console.log(imgElements);
+  
+
+//STEP 1B: Check to make sure all the DOM elements are loaded in the array
+  console.log(imgURLS.length, imgElements.length);
+  if (imgURLS.length === imgElements.length) {  //do imgURLS-1 to skip the last weird .txt file
+    imgsLoaded = true;
+    console.log("Images have loaded");
+  }
+
 
 //STEP 2: for every square in our grid, we're going to create an image tile from our DOM elements and draw it to the on screen canvas
- 
+ if (imgsLoaded = true) {
   var i = 0; //variable to keep track of what image we're on in the array of images
   for (var x =0; x < colNum; x++) { //for every column
     for (var y =0; y < rowNum; y++) { //for every row
@@ -63,30 +70,24 @@ function setup() {
       if (i === imgElements.length) { 
         i = 0;
       }
-      console.log(imgElements[i], x, y);
+      // console.log(imgElements[i], x, y);
     }
   }
 
+}
 }
 
    
 
 function processImage(imgElt, x, y) {
 
-  //function makeTile(){
-    //for every image in the array of image DOM elements
-    //first we draw the image to an offscreen canvas
-    
     var graphics = createGraphics(w, h); //draw the graphics canvas at the same dimensions as the canvas in the browser so they mirror each other
     graphics.image(imgElt, 0, 0, graphics.width, graphics.height);
-    console.log(imgElt);
+    // console.log(imgElt);
 
     x = x*tileSize; //take a tile and assign it to a place in the canvas in our browser
     y = y*tileSize;
     mycopy(graphics, x, y, tileSize, tileSize, x, y, tileSize, tileSize);
-
-
-   //}
 
 }
 
